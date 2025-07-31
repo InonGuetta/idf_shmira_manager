2a4f0916712837c2e0d8b6919192fbbfc71c693c
Inon Guetta <inon16guetta@gmail.com>
2025-08-01 01:21:14 +0300

    finish_design_table_user

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,31 +1,25 @@
 import { Column, DataType, Model, Table } from "sequelize-typescript";
 import { role } from "./enum/role.enum";
 
-@Table
+@Table({ timestamps: false }) 
 export class Users extends Model {
 
-    @Column({
-        type: DataType.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    })
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  })
+  user_id: number;
 
-    user_id: number;
+  @Column
+  user_name: string;
 
-    @Column
-    user_name: string;
+  @Column
+  password: string;
 
-    @Column
-    password: string;
-
-    @Column({
-        // type:DataType.ENUM(...Object(role)),
-        defaultValue: role.soldier
-    })
-    role: role
-
+  @Column({
+    type: DataType.ENUM(...Object.values(role)),
+    defaultValue: role.soldier
+  })
+  role: role;
 }
-
-
-
-
